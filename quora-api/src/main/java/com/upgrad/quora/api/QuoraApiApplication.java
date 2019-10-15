f93e7b54097f93dc31ca52462376e765c46338aa
dthakor <dthakor@vmware.com>
2019-10-15 11:35:54 +0530

    signup user

---
+++ quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
@@ -0,0 +1,21 @@
+package com.upgrad.quora.api;
+
+import com.upgrad.quora.service.ServiceConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+/**
+ * A Configuration class that can declare one or more @Bean methods and trigger auto-configuration and component scanning.
+ * This class launches a Spring Application from Java main method.
+ */
+@SpringBootApplication
+@Import(ServiceConfiguration.class)
+public class QuoraApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(QuoraApiApplication.class, args);
+    }
+}
+
